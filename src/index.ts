3393a6eb680dee9ba86da99b91f201b536c3cb60
Taras Mankovski <tarasm@gmail.com>
2017-08-20 09:41:54 +0530

    fix(exports) Added missing exports

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,22 @@ import reduceObject from './reduceObject';
 import mapObject from './mapObject';
 import filterObject from './filterObject';
 import eachProperty from './eachProperty';
+import map from './map';
+import filter from './filter';
+import reduce from './reduce';
+import push from './push';
+import * as Interfaces from './Interfaces';
 
-export { eachProperty, filterObject, mapObject, reduceObject, set, get };
+export {
+  eachProperty,
+  filterObject,
+  mapObject,
+  reduceObject,
+  set,
+  get,
+  map,
+  filter,
+  reduce,
+  push,
+  Interfaces,
+};
