24917b99995b11c5c706e6674446dbe645421e34
Taras Mankovski <tarasm@gmail.com>
2017-08-27 21:57:27 +0530

    get returns any

--- src/get.ts
+++ src/get.ts
@@ -3,7 +3,7 @@ import { lensPath, view } from 'ramda';
 export default function get(
   path: Array<string | number>,
   data: {} | Array<any>,
-) {
+): any {
   let lens = lensPath(path);
   return view(lens, data);
 }
